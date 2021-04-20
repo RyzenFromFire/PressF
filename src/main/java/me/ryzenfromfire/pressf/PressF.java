8977904dd93d2a608ab8c3fa4c6288c1c929b653
RyzenFromFire <ryzenfromfire@gmail.com>
2021-04-19 22:16:16 -0400

    Implemented base features
    added startup message
    added fCount hashmap
    added pressf command
    added viewf command
    added commands to plugin.yml
    version to 0.0.2

--- src/main/java/me/ryzenfromfire/pressf/PressF.java
+++ src/main/java/me/ryzenfromfire/pressf/PressF.java
@@ -1,17 +1,52 @@
 package me.ryzenfromfire.pressf;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public final class PressF extends JavaPlugin {
+
+    HashMap<UUID, Integer> fCount = new HashMap<UUID, Integer>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Pressing the start button (not F).");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (command.getName().equals("pressf")) {
+            Player player = (Player) sender;
+            Player target = Bukkit.getPlayer(args[0]);
+
+            if (target == null) {
+                //TODO: Replace "player" with a UUID of the player who sent the last message before the sender.
+                target = player;
+            }
+            //if player is listed, set count to their current fCount, if not, initialize with 0
+            int count = fCount.getOrDefault(target.getUniqueId(), 0);
+
+            //increment target's fCount
+            fCount.put(target.getUniqueId(), count + 1);
+        }
+
+        if (command.getName().equals("viewf")) {
+            Player player = (Player) sender;
+            player.sendMessage("[PressF] You have " + fCount.get(player.getUniqueId()) + "F's.");
+        }
+
+        return false;
     }
 }
