c2b12e04bcfa72dedf2e7da99d3d6c14f0813f0e
RyzenFromFire <16062019+RyzenFromFire@users.noreply.github.com>
2021-05-10 08:37:39 -0400

    Added proper dependencies

--- src/main/java/me/ryzenfromfire/pressf/PressF.java
+++ src/main/java/me/ryzenfromfire/pressf/PressF.java
@@ -14,7 +14,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
